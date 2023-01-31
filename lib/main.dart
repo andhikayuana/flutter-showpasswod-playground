7afd805f1c9a7c307bdf52d0aafddb5c85cae32b
Andhika Yuana <andhikayuana@gmail.com>
2023-01-31 12:39:59 +0700

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class FlutterShowPasswordApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Show Password Playground',
       theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
       home: const LoginScreen(),
     );
   }
